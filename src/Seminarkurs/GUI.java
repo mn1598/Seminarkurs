b269117ace2ac2e32a8247307d66dac362255086
Marcel <marceln98@web.de>
2016-01-05 16:52:57 +0100

    First Commit

---
+++ src/Seminarkurs/GUI.java
@@ -0,0 +1,25 @@
+package Seminarkurs;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+
+public class GUI extends Application{
+
+	public static void main(String[] args){
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
